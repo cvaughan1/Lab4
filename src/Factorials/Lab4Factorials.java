7fb4813c1d748441df00c44c91c42d61c3a9c412
Clarkston Vaughan <clarkvaughan111@yahoo.com>
2016-09-14 11:21:45 -0400

    Lab 4 new Recursion commit

--- src/Factorials/Lab4Factorials.java
+++ src/Factorials/Lab4Factorials.java
@@ -3,18 +3,55 @@ package Factorials;
 import java.util.Scanner;
 
 public class Lab4Factorials {
+
 	public static void main(String[] args) {
-
-		System.out.println("Please enter a number fron 1 to 10");
+			
+		//Recursion strategy
+		
+		System.out.println("Welcome to the Grand Circus Factorial Calculator!");
+		System.out.println();
+		String choice = "y";
+		
+		while (choice.equals("y")) {
+		
+		System.out.println("Enter an integer between 0 and 10");
+		
 		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		
+		int result = factorial(n);
+		
+	
+		
+		
+		System.out.println("The factorial of " + n + " is " + result);
+		
+		
+		
+	// Continue block
 
-		System.out.print("here: ");
-		long num = sc.nextLong();
+	System.out.print("Continue? (y/n): ");
+	choice = sc.next();
+	System.out.println();
 
-		int result = 1;
-		for (int i = 1; i <= num; i++) {
-			result = result * i;
-		}
-		System.out.println("The factorial of " + num + " is " + result);
-	}
-}
+		
+				}	
+		
+		
+
+}//pvsm
+
+
+private static int factorial(int n) {
+		if (n == 0) {
+			return 1;
+		} else {
+			return n * factorial(n - 1);
+	
+		}//else
+}//end method
+
+
+
+}//class
